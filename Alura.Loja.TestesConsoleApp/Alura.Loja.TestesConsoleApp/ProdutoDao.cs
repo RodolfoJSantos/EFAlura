40d7fa2e2d55ae274c13545d5ed211539cc02946
rodolfo santos <rodjsant@outlook.com>
2019-08-29 21:44:43 -0300

    commit inicial

---
+++ Alura.Loja.TestesConsoleApp/Alura.Loja.TestesConsoleApp/ProdutoDao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.TestesConsoleApp
+{
+    public class ProdutoDao : IDisposable
+    {
+        public void Dispose()
+        {
+            this.conexao.Close();
+        }
+
+        private IDbConnection conexao;
+
+        public ProdutoDao()
+        {
+            this.conexao = new SqlConnection("");
+            this.conexao.Open();
+        }
+
+        internal void Adicionar(Produto p)
+        {
+            try
+            {
+                IDbCommand insertCmd = conexao.CreateCommand();
+                insertCmd.CommandText = "INSERT INTO Produtos (Nome, Categoria, Preco) VALUES (@nome, @categoria, @preco)";
+
+                IDbDataParameter paramNome = new SqlParameter("nome", p.Nome);
+                insertCmd.Parameters.Add(paramNome);
+
+                IDbDataParameter paramCategoria = new SqlParameter("categoria", p.Categoria);
+                insertCmd.Parameters.Add(paramCategoria);
+
+                IDataParameter paramPreco = new SqlParameter("preco", p.Preco);
+                insertCmd.Parameters.Add(paramPreco);
+
+                insertCmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new SystemException(e.Message, e);
+            }
+        }
+    }
+}
